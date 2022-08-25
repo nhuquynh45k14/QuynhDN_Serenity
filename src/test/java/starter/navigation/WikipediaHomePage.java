f9b7af6bcef5534abbcb75525c15df2b64b8b8a2
nhuquynh45k14 <quynhbabe123456789@gmail.com>
2022-08-25 11:33:24 +0700

    abc

--- src/test/java/starter/navigation/WikipediaHomePage.java
+++ src/test/java/starter/navigation/WikipediaHomePage.java
@@ -4,6 +4,6 @@ import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
 @DefaultUrl("https://wikipedia.org")
+public class WikipediaHomePage extends PageObject {
 
-
-public class WikipediaHomePage extends PageObject {}
+}
